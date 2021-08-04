aac193ec189b7ae9c50f1d9a67f7c35d88d2df73
Mihailob5017 <mihailob5017@gmail.com>
2021-08-04 12:11:31 +0200

    created all the needed components and connected them

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Components
+import Post from './Post/Post';
+const Posts = () => {
+	return (
+		<div>
+			<Post />
+			<Post />
+		</div>
+	);
+};
+
+export default Posts;
