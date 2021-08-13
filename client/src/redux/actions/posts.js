8aa344e00357732b62bbf883a9f9b5bc16af3133
Mihailob5017 <mihailob5017@gmail.com>
2021-08-13 12:18:32 +0200

    added the constants

--- client/src/redux/actions/posts.js
+++ client/src/redux/actions/posts.js
@@ -1,11 +1,17 @@
 import * as api from '../../api/index';
-
+import {
+	GET_POSTS,
+	CREATE_POST,
+	UPDATE_POST,
+	DELETE_POST,
+	LIKE_POST,
+} from '../constats';
 // Functions that return
 
 export const getPosts = () => async (dispatch) => {
 	try {
 		const { data } = await api.fetchPosts();
-		dispatch({ type: 'FETCH_ALL', payload: data });
+		dispatch({ type: GET_POSTS, payload: data });
 	} catch (error) {
 		console.log(error.message);
 	}
@@ -14,7 +20,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
 	try {
 		const res = await api.createPost(post);
-		dispatch({ type: 'CREATE_POST', payload: res.data });
+		dispatch({ type: CREATE_POST, payload: res.data });
 	} catch (error) {
 		console.log(error);
 	}
@@ -23,7 +29,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
 	try {
 		const { data } = await api.updatePost(id, post);
-		dispatch({ type: 'UPDATE_POST', payload: data });
+		dispatch({ type: UPDATE_POST, payload: data });
 	} catch (error) {
 		console.log(error);
 	}
@@ -32,7 +38,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
 	try {
 		await api.deletePost(id);
-		dispatch({ type: 'DELETE_POST', payload: id });
+		dispatch({ type: DELETE_POST, payload: id });
 	} catch (error) {
 		console.log(error);
 	}
@@ -41,7 +47,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
 	try {
 		const { data } = await api.likePost(id);
-		dispatch({ type: 'LIKE_POST', payload: data });
+		dispatch({ type: LIKE_POST, payload: data });
 	} catch (error) {
 		console.log(error);
 	}
--- client/src/redux/reducers/posts.js
+++ client/src/redux/reducers/posts.js
@@ -1,17 +1,24 @@
+import {
+	GET_POSTS,
+	CREATE_POST,
+	UPDATE_POST,
+	LIKE_POST,
+	DELETE_POST,
+} from '../constats';
 export const postsReducer = (state = [], action) => {
 	switch (action.type) {
-		case 'FETCH_ALL':
+		case GET_POSTS:
 			return [...state, ...action.payload];
-		case 'CREATE_POST':
+		case CREATE_POST:
 			return [...state, action.payload];
-		case 'UPDATE_POST':
+		case UPDATE_POST:
 			return state.map((el) =>
 				el._id === action.payload._id ? action.payload : el
 			);
-		case 'DELETE_POST':
+		case DELETE_POST:
 			return state.filter((el) => el._id !== action.payload);
 
-		case 'LIKE_POST':
+		case LIKE_POST:
 			return state.map((el) =>
 				el._id === action.payload._id ? action.payload : el
 			);
