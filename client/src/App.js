23bd81ae177d8618dbb611780053e16c97de63f1
Mihailob5017 <mihailob5017@gmail.com>
2021-08-13 16:02:20 +0200

    made it responsive

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,12 @@ const App = () => {
 			</AppBar>
 			<Grow in>
 				<Container>
-					<Grid container justifyContent='space-between' alignItems='stretch'>
+					<Grid
+						container
+						className={classes.mainContainer}
+						justifyContent='space-between'
+						alignItems='stretch'
+					>
 						<Grid item xs={12} sm={7}>
 							<Posts setCurrentId={setCurrentId} />
 						</Grid>
