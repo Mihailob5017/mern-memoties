086ae6e7ee0f269842c08de52ca589ec64da7ce8
Mihailob5017 <mihailob5017@gmail.com>
2021-08-17 14:23:36 +0200

    added the Sign In Controler backend

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,1 +1,35 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+
+export const signIn = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		const currentUser = await User.findOne({ email });
+		if (!currentUser)
+			return res.status(404).json({ message: 'User does not exist' });
+
+		const isPasswordCorrect = await bcrypt.compare(
+			password,
+			currentUser.password
+		);
+		if (!isPasswordCorrect)
+			return status(400).json({ message: 'Invalid Credentials' });
+
+		const token = jwt.sign(
+			{ email: currentUser.email, id: currentUser._id },
+			'test_secret',
+			{ expiresIn: '1h' }
+		);
+
+		res.status(200).json({ result: currentUser, token });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong', error });
+	}
+};
+
+export const signUp = async (req, res) => {
+	try {
+	} catch (error) {}
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {} from '../controllers/user.js';
+import { signIn, signUp } from '../controllers/user.js';
 const router = express.Router();
 
-// router.get('/', );
+router.post('/signin', signIn);
+router.post('/signin', signUp);
 
 export default router;
