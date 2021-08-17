208c73c87bb2fcd97ebfcc62b4c14c5c6f2289b8
Mihailob5017 <mihailob5017@gmail.com>
2021-08-17 15:18:11 +0200

    sign up controller finished

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -30,6 +30,31 @@ export const signIn = async (req, res) => {
 };
 
 export const signUp = async (req, res) => {
+	const { email, password, firstName, lastName, confirmPassword } = req.body;
+
 	try {
-	} catch (error) {}
+		const currentUser = await User.findOne({ email });
+		if (currentUser)
+			return res.status(404).json({ message: 'User  already exists' });
+		if (password !== confirmPassword)
+			return res.status(400).json({ message: 'Passwords dont match' });
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+
+		const result = await User.create({
+			email,
+			password: hashedPassword,
+			name: `${firstName} ${lastName}`,
+		});
+
+		const token = jwt.sign(
+			{ email: result.email, id: result._id },
+			'test_secret',
+			{ expiresIn: '1h' }
+		);
+
+		res.status(200).json({ result, token });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong', error });
+	}
 };
